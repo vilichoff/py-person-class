036223afadaec421e808ebe2eb81b7b3a1f87c97
Vladislav <ilichoff88@gmail.com>
2025-06-19 23:13:23 +0300

    'Solution.01'

--- app/main.py
+++ app/main.py
@@ -8,6 +8,8 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
+    Person.people = {}
+
     person_list = []
 
     for person_data in people:
@@ -20,10 +22,14 @@ def create_person_list(people: list) -> list:
         name = person_data["name"]
         person = Person.people[name]
 
-        if "wife" in person_data and person_data["wife"]:
-            person.wife = Person.people[person_data["wife"]]
+        if "wife" in person_data:
+            wife_name = person_data["wife"]
+            if wife_name and wife_name in Person.people:
+                person.wife = Person.people[wife_name]
 
-        if "husband" in person_data and person_data["husband"]:
-            person.husband = Person.people[person_data["husband"]]
+        if "husband" in person_data:
+            husband_name = person_data["husband"]
+            if husband_name and husband_name in Person.people:
+                person.husband = Person.people[husband_name]
 
     return person_list
